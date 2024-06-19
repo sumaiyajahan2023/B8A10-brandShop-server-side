14b27d9b4659a820d745f31b2fb2e8c238556b73
sumaiya <sumaiya.cse20@gmail.com>
2024-06-19 17:15:38 +0600

    add products connected with local storage

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('hello')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+let products = [] //in memory storage
+
+async function run() {
+    try{
+        app.get('/products', async(req, res) => {
+            res.send(products)
+        })
+        
+        app.post('/products', async(req, res) => {
+            const product = req.body;
+            products.push(product)
+            console.log(product)
+            res.send(products)
+        })
+    }
+    catch{
+        throw new Error('Something went wrong on the server!', error)
+    }
+}
+run().catch(console.log)
+
+app.get('/', (req, res) => {
+    res.send('server running properly')
+})
+
+app.listen(port, ()=>{
+    console.log(`Running on port:${port}`)
+})

849183a23b5581d2e5c4c2cb43fa3b8209b7daeb
sumaiya <sumaiya.cse20@gmail.com>
2024-06-20 11:44:48 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,36 +1,63 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-let products = [] //in memory storage
+// let products = []; //in memory storage
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lbhoupe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
-    try{
-        app.get('/products', async(req, res) => {
-            res.send(products)
-        })
-        
-        app.post('/products', async(req, res) => {
-            const product = req.body;
-            products.push(product)
-            console.log(product)
-            res.send(products)
-        })
-    }
-    catch{
-        throw new Error('Something went wrong on the server!', error)
-    }
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    app.get("/products", async (req, res) => {
+      const productsCollection = client.db("brand_shop").collection("products");
+      const cursor = productsCollection.find()
+      const result = await cursor.toArray()
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      //   products.push(product);
+      //   console.log(product);
+      const productsCollection = client.db("brand_shop").collection("products");
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
+  } catch {
+    throw new Error("Something went wrong on the server!", error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
-run().catch(console.log)
+run().catch(console.log);
 
-app.get('/', (req, res) => {
-    res.send('server running properly')
-})
+app.get("/", (req, res) => {
+  res.send("server running properly");
+});
 
-app.listen(port, ()=>{
-    console.log(`Running on port:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Running on port:${port}`);
+});

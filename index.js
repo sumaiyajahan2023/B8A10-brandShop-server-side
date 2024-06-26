57ffa39c6eb51e943f9129a8793b710d6fe02228
sumaiya <sumaiya.cse20@gmail.com>
2024-06-26 21:37:47 +0600

    cleared unused code

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lbhoupe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,

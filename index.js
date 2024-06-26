6e5dffd4e7bf88047c31a7e1c6d3c5f43f7fc411
sumaiya <sumaiya.cse20@gmail.com>
2024-06-26 16:52:19 +0600

    more error handled and vercel.json updated

--- index.js
+++ index.js
@@ -144,4 +144,8 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
 module.exports = app;
